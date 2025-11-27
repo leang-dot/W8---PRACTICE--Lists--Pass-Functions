7c5dc40174ff565457babce075b577f7ad23737c
leang-dot <ounleang180@gmail.com>
2025-11-27 07:29:17 +0700

    push

--- ex2/lib/data/profile_data.dart
+++ ex2/lib/data/profile_data.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import '../model/profile_tile_model.dart';
 
-ProfileData ronanProfile = ProfileData(
-  name: "Ronan",
-  position: "Flutter Developer",
-  avatarUrl: 'assets/ronan.png',
+ProfileData meProfile = ProfileData(
+  name: "Leang",
+  position: "Super Code",
+  avatarUrl: 'assets/me.jpg',
   tiles: [
     TileData(icon: Icons.phone, title: "Phone Number", value: "+123 456 7890"),
     TileData(icon: Icons.location_on, title: "Address", value: "123 Cambodia"),
-    TileData(icon: Icons.email, title: "Mail", value: "ronan.ogogr@cadt.edu"),
+    TileData(icon: Icons.email, title: "Mail", value: "leangochii@tmail.co"),
   ],
 );
 
-ProfileData kwanProfile = ProfileData(
-  name: "Kwan",
-  position: "FullStack Developer At RaingKosal",
-  avatarUrl: "assets/kwan.png",
+ProfileData ronaldoProfile = ProfileData(
+  name: "Ronaldo",
+  position: "Super Penalty",
+  avatarUrl: "assets/ronaldo.jpg",
   tiles: [
     TileData(icon: Icons.phone, title: "Phone Number", value: "+855 123 1010"),
     TileData(icon: Icons.location_on, title: "Address", value: "123 Cambodia"),
-    TileData(icon: Icons.email , title: "Mail" , value: "yoljlomby.kwan@cadt.edu"),
+    TileData(icon: Icons.email , title: "Mail" , value: "yessss@cambo.cypher"),
   ],
 );
-List profileData = [ronanProfile , kwanProfile];
+List profileData = [meProfile , ronaldoProfile];
